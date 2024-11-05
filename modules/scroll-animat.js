ff5a4e48509d5226dbf9ee40b69b7154420d0679
Andre-Lucho <andretlucho@gmail.com>
2024-11-05 11:59:59 -0300

    ref scroll-animat.js into Class + new instances

--- modules/scroll-animat.js
+++ modules/scroll-animat.js
@@ -1,27 +1,47 @@
 export default class AnimatToScroll {
   constructor(sections) {
     this.sections = document.querySelectorAll(sections);
-    this.halfWindow = window.innerHeight * 0.5;
-    this.scrollAnimt = this.scrollAnimt.bind(this);
+    this.halfWindow = window.innerHeight * 0.4;
+    this.checkDistance = this.checkDistance.bind(this);
   }
-  scrollAnimt() {
-    this.sections.forEach((section) => {
-      // pega a distância que a tag com a classe "[data-anime='scroll]"está do topo do site
-      const sectionTop = section.getBoundingClientRect().top;
-      const isSectionVisible = sectionTop - this.halfWindow < 0; //true or false
 
-      // Fazer verificação ao scrollar:
-      // scrolar p baixo, passando o elemento com a class "[data-anime='scroll]"
-      if (isSectionVisible) {
-        section.classList.add("actived");
-      } else if (section.classList.contains("actived")) {
-        section.classList.remove("actived");
+  // Pega a distância de cada section em relação ao topo do site
+  getDistance() {
+    // quero o retorno da iteração para guardar no objeto !
+    // observar que this.sections = nodeList --> tenho q tranf em array para fazer .map(permite o retorno)
+    this.distance = [...this.sections].map((section) => {
+      // ou  Array.from(this.sections)
+      // agora, pega a distância fixa das sections até o topo do site 1 única vez ao entrar no site
+      const sectionTop = section.offsetTop;
+      return {
+        sectionTag: section,
+        offsetTop: Math.floor(sectionTop - this.halfWindow),
+      };
+    });
+  }
+
+  // Fazer verificação do scroll em relaçao ao retorno(objeto) de getDistance()
+  checkDistance() {
+    this.distance.forEach((item) => {
+      if (window.scrollY > item.offsetTop) {
+        item.sectionTag.classList.add("actived");
+      } else if (item.sectionTag.classList.contains("actived")) {
+        item.sectionTag.classList.remove("actived");
       }
     });
   }
 
   init() {
-    this.scrollAnimt();
-    window.addEventListener("scroll", this.scrollAnimt);
+    if (this.sections.length) {
+      this.getDistance();
+      this.checkDistance();
+      window.addEventListener("scroll", this.checkDistance);
+    }
+    return this;
+  }
+
+  // Remove o event de animação do scroll (caso necessário)
+  stopAnimScroll() {
+    window.removeEventListener("scroll", this.checkDistance);
   }
 }
